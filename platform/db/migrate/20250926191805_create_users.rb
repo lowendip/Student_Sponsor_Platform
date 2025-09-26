c4d7438d4ebaf65222fce8685a2a2cc66624c574
lowendip <lowendipaula@live.com>
2025-09-26 16:27:24 -0400

    Added users to db and added a user to the seeds file

---
+++ platform/db/migrate/20250926191805_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table Users do |t|
+      t.string :name, default: ""
+      t.string :organization, default: ""
+      t.string :contact, default: ""
+      t.string :domains, array: true, default: []
+      t.string :role, default: ""
+      t.string :username
+      t.string :password
+      t.timestamps
+    end
+  end
+end
--- platform/db/seeds.rb
+++ platform/db/seeds.rb
@@ -7,3 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.find_or_create_by!(name: "Sponsor", organization: "UMBC", contact: "fake@email.com", domains: ["AI","Web Development"], role: "sponsor", password: "test")
