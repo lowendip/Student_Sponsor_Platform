222424093771742cbf9ad771b398b564d96a0f7f
lowendip <lowendipaula@live.com>
2025-11-30 20:59:42 -0500

    Made config changes for the deployment storage

--- Gemfile
+++ Gemfile
@@ -56,6 +56,11 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+  gem 'railroady'
+end
+
+group :production do
+  gem "aws-sdk-s3", require: false
 end
 
 group :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # config.asset_host = "http://assets.example.com"
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
@@ -79,7 +79,7 @@ Rails.application.configure do
     open_timeout:    5,
     read_timeout:    5
   }
-
+ 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
