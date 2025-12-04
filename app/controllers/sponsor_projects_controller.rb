83eb04bb6441ed1ac4a0c43c6adcea0274a23c20
lowendip <lowendipaula@live.com>
2025-12-04 17:49:56 -0500

    Made minor changes to search utility for projects (including adding search by student/organization)

--- app/controllers/sponsor_projects_controller.rb
+++ app/controllers/sponsor_projects_controller.rb
@@ -1,9 +1,11 @@
 class SponsorProjectsController < ApplicationController 
     def index
       params[:q] = {} if params[:q].blank?
-      @q = Project.belongs_to_sponsor.active_projects.where("expiration >= ?", DateTime.now).where(status:"Visible").order(updated_at: :desc).ransack(params[:q])
+      project_ids = Project.belongs_to_sponsor.active_projects.pluck(:id)
+      @q = Project.joins(:user).where(id:project_ids).where("expiration >= ?", DateTime.now).where(status:"Visible").order(updated_at: :desc).ransack(params[:q])
       @projects = @q.result
       @q_name_cont = params[:q][:name_cont]
+      @q_user_organization_cont = params[:q][:user_organization_cont]
       @q_domains_id_in = params[:q][:domains_id_in]
       @domains = Domain.all
     end
--- app/controllers/student_projects_controller.rb
+++ app/controllers/student_projects_controller.rb
@@ -1,9 +1,11 @@
 class StudentProjectsController < ApplicationController 
     def index
       params[:q] = {} if params[:q].blank?
-      @q = Project.belongs_to_student.active_projects.where("expiration >= ?", DateTime.now).where(status:"Visible").order(updated_at: :desc).ransack(params[:q])
+      project_ids = Project.belongs_to_student.active_projects
+      @q = Project.where(id:project_ids).where("expiration >= ?", DateTime.now).where(status:"Visible").order(updated_at: :desc).ransack(params[:q])
       @projects = @q.result
       @q_name_cont = params[:q][:name_cont]
+      @q_user_name_cont = params[:q][:user_name_cont]
       @q_domains_id_in = params[:q][:domains_id_in]
       @domains = Domain.all
     end
