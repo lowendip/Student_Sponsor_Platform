fd2f3aa7116b6c4ca0dbdcf2d9c14dc8197077e1
lowendip <lowendipaula@live.com>
2025-12-07 12:17:35 -0500

    small bug fix to front page display

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
     #This is used to determine which projects will be displayed on the home page.
     #This is only used if the user is logged in
     #This priortizes the 9 most recent projects and it will only display projects with at least one image.
-    @projects = Project.joins(:images_blobs).where(active_storage_blobs: { content_type: ["image/png", "image/jpeg"] }).order(created_at: :desc).limit(9)
+    project_ids = Project.active_projects.pluck(:id)
+    @projects = Project.joins(:images_blobs).where(id:project_ids).where(active_storage_blobs: { content_type: ["image/png", "image/jpeg"] }).order(created_at: :desc).limit(9)
   end
 end
